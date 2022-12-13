0c5fb912a52652123b56240a0c3f533acf744150
abhijeet2929 <116516095+abhijeet2929@users.noreply.github.com>
2022-12-13 18:05:53 +0530

    add updated config file

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 
 PORT = 7007
 
-MODEL_FILE_PATH=r"E:\ABHIJEET\2.Project_file\Project_app\LogisticModelIris.pkl"
-JSON_FILE_PATH=r"E:\ABHIJEET\2.Project_file\Project_app\Project_data.json"
+# MODEL_FILE_PATH=r"E:\ABHIJEET\2.Project_file\Project_app\LogisticModelIris.pkl"
+# JSON_FILE_PATH=r"E:\ABHIJEET\2.Project_file\Project_app\Project_data.json"
+
+MODEL_FILE_PATH=os.path.join("Project_app","LogisticModelIris.pkl")
+JSON_FILE_PATH=os.path.join("Project_app","Project_data.json")
+
